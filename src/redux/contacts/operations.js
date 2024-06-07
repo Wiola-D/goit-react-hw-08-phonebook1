8bde95c18030104ab20f44202a91d89127b8cb6f
Wiola-D <wioletta.d.work@gmail.com>
2024-06-07 09:21:12 +0200

    add redux

---
+++ src/redux/contacts/operations.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async ({ name, number }, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const existingContact = state.contacts.contacts.find(
+      contact => contact.name.toLowerCase() === name.trim().toLowerCase()
+    );
+    if (existingContact) {
+      return thunkAPI.rejectWithValue(`${name} is already in contacts!`);
+    }
+    try {
+      const response = await axios.post('/contacts', {
+        name,
+        number,
+      });
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
